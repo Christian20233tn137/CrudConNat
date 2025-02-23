51e2cadc0af046e999d77206f2bb59bffa2b0781
Christian20233tn137 <20233tn137@utez.edu.mx>
2025-02-22 20:24:55 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const personasRoutes = require("./routes/persona.routes");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use("/api/personas", personasRoutes);
+
+// Conexion a la base de datos
+
+mongoose.connect(
+  "mongodb+srv://20233tn137:Sca0K7DkIhgwpNs1@chriscloud.wkdec.mongodb.net/inventario-db?retryWrites=true&w=majority&appName=ChrisCloud"
+).then(() => {
+  console.log("Conectado a la base de datos");
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en  https://localhost:${(PORT)}`);
+  });
+}).catch((error) => {
+  console.log("Error al conectar a la base de datos", error);
+});
